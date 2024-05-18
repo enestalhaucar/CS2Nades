613dee0b33cc4c40318022d9d83d70c391623be8
Enes Talha Uçar <talhauenes@gmail.com>
2024-05-18 16:07:26 +0300

    Callout View Added

---
+++ CS2Nades/View/CalloutView.swift
@@ -0,0 +1,18 @@
+//
+//  CalloutView.swift
+//  CS2Nades
+//
+//  Created by Enes Talha Uçar  on 18.05.2024.
+//
+
+import SwiftUI
+
+struct CalloutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalloutView()
+}
